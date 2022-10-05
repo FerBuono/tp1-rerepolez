7019b93f638c1f01b25966dfcd3b0afe6970b9df
Fran <fdelucaa@fi.uba.ar>
2022-10-05 18:03:10 -0300

    Cambio a partido_implementacion

--- votos/partido_implementacion.go
+++ votos/partido_implementacion.go
@@ -13,6 +13,9 @@ type partidoImplementacion struct {
 }
 
 type partidoEnBlanco struct {
+	votosPresid int
+	votosGober  int
+	votosIntend int
 }
 
 func CrearPartido(nombre string, candidatos [CANT_VOTACION]string) Partido {
@@ -28,17 +31,19 @@ func CrearPartido(nombre string, candidatos [CANT_VOTACION]string) Partido {
 }
 
 func CrearVotosEnBlanco() Partido {
-	return nil
+	blanco := new(partidoEnBlanco)
+	blanco.votosPresid = 0
+	blanco.votosGober = 0
+	blanco.votosIntend = 0
+	return blanco
 }
 
 func (partido *partidoImplementacion) VotadoPara(tipo TipoVoto) {
 	if tipo == "Presidente" {
 		partido.votosPresid += 1
-	}
-	if tipo == "Gobernador" {
+	} else if tipo == "Gobernador" {
 		partido.votosGober += 1
-	}
-	if tipo == "Intendente" {
+	} else if tipo == "Intendente" {
 		partido.votosIntend += 1
 	}
 }
@@ -55,9 +60,22 @@ func (partido partidoImplementacion) ObtenerResultado(tipo TipoVoto) string {
 }
 
 func (blanco *partidoEnBlanco) VotadoPara(tipo TipoVoto) {
-
+	if tipo == "Presidente" {
+		blanco.votosPresid += 1
+	} else if tipo == "Gobernador" {
+		blanco.votosGober += 1
+	} else if tipo == "Intendente" {
+		blanco.votosIntend += 1
+	}
 }
 
 func (blanco partidoEnBlanco) ObtenerResultado(tipo TipoVoto) string {
+	if tipo == "Presidente" {
+		return fmt.Sprintf("El Candidato %s tiene %i votos", partido.presidente, partido.votosPresid)
+	} else if tipo == "Gobernador" {
+		return fmt.Sprintf("El Candidato %s tiene %i votos", partido.gobernador, partido.votosGober)
+	} else if tipo == "Intendente" {
+		return fmt.Sprintf("El Candidato %s tiene %i votos", partido.intendente, partido.votosIntend)
+	}
 	return ""
 }
